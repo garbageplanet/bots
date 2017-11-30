ff90b235cb072421074139ae43006d6696a686c4
adriennn <adrien.vetterli@gmail.com>
2017-11-30 22:39:05 +0200

    added csp

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
+const express      = require('express')
+const path         = require('path')
+const logger       = require('morgan')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-
-const index = require('./routes/index')
-
-const app = express()
+const bodyParser   = require('body-parser')
+const csp          = require('helmet-csp')
+const index        = require('./routes/index')
+const app          = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.set('trust proxy', '127.0.0.1')
 
+app.use(compression())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(csp({
+  directives: {
+      defaultSrc: [  "'self'"
+                   , 'https://api.telegram.org'
+                  ]
+
+    , connectSrc: [  "'self'"
+                  , 'http://127.0.0.1:7000'
+                  , 'https://' + process.env.APP_WEB_URL
+                  , 'https://api.telegram.org'
+                ]
+
+    , scriptSrc: [  "'self'" ]
+
+    , imgSrc: [  "'self'"
+               , 'https://api.telegram.org'
+            ]
+  }
+}))
 
 app.use('/bots', index)
 
