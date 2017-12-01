26ca399e5434a792acb43101bec127a610ac54e1
adriennn <adrien.vetterli@gmail.com>
2017-12-01 20:43:07 +0200

    ...

--- app.js
+++ app.js
@@ -54,8 +54,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500)
-  res.sendStatus(err.status)
+  res.sendStatus(err.status || 500)
 })
 
 console.log('Bots app started on port 7000')
