ce83f7d42dc27411951667b389abd044e5acc9d3
adriennn <adrien.vetterli@gmail.com>
2018-02-01 23:21:54 +0200

    refactoring

--- middleware/type/index.js
+++ middleware/type/index.js
@@ -6,32 +6,16 @@ module.exports = (req, res, next) => {
 //   let type = req.url.substr(1, 2)
 
   let type = req.url.slice(1).split('/').shift()
-  console.log(req.url.split('/'))
-  console.log(type)
   res.locals.bot_type = type
 
-  // switch (type) {
-  //
-  //   case 'me' : res.locals.bot_type = 'messenger'
-  //
-  //               res.locals.sender = req.body.entry[0].messaging[0].sender.id
-  //   break
-  //   case 'te' : res.locals.bot_type = 'telegram'
-  //               res.locals.sender = req.body.message.from.id
-  //   break
-  //   case 'tw' : res.locals.bot_type = 'twitter'
-  //   break
-  //   case 'wh' : res.locals.bot_type = 'whatsapp'
-  //   break
-  //   case 'vi' : res.locals.bot_type = 'viber'
-  //   break
-  //   case 'we' : res.locals.bot_type = 'wechat'
-  //   break
-  //   case 'bb' : res.locals.bot_type = 'bbm'
-  //   break
-  //   default   : res.locals.bot_type = null
-  //
-  // }
+  switch (type) {
+
+    case 'me' : res.locals.sender = req.body.entry[0].messaging[0].sender.id
+    break
+    case 'te' : res.locals.sender = req.body.message.from.id
+    break
+    default   : res.locals.sender = null
+  }
 
   //console.log(`Sender id is ${res.locals.sender}`)
   console.log(`Sender is on ${res.locals.bot_type}`)
