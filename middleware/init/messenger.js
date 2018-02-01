ee906223e0614c72e5d69337fa0bad450397fa12
adriennn <adrien.vetterli@gmail.com>
2018-02-01 22:48:41 +0200

    testing messenger

--- middleware/init/messenger.js
+++ middleware/init/messenger.js
@@ -15,6 +15,14 @@ module.exports = (req, res, next) => {
     return next(error)
   }
 
+  if ( !req.body.entry[0].messaging[0].message.attachments ) {
+
+    console.log('No attachments.', req.body)
+
+    let error = new Error('I am a simple minde. Send pic.')
+    return next(error)
+  }
+
   if ( req.body.entry[0].messaging[0].message.attachments[0].type !== 'file') {
 
     console.log('Image not sent as file.', req.body)
@@ -22,6 +30,8 @@ module.exports = (req, res, next) => {
     let error = new Error('Make sure to attach the image you are sending as a file and not as a photo.')
     return next(error)
   }
+
+
 
   // TODO https://developers.facebook.com/docs/messenger-platform/send-messages/sender-actions
   // TODO https://developers.facebook.com/docs/messenger-platform/send-messages/#messaging_types
